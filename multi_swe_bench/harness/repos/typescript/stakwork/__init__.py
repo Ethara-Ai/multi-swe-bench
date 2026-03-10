77cf2406ba4a9ef973aa42722dc7458885270f7c
Rakshit Pandey <rakshit.pandey@ethara.ai>
2026-03-10 16:43:20 +0530

    stakwork init file fix

--- multi_swe_bench/harness/repos/typescript/stakwork/__init__.py
+++ multi_swe_bench/harness/repos/typescript/stakwork/__init__.py
@@ -1,2 +1,1 @@
-from multi_swe_bench.harness.repos.typescript.stakwork.sphinx_tribes_frontend_63_to_36 import *
-from multi_swe_bench.harness.repos.typescript.stakwork.sphinx_tribes_frontend_1534_to_95 import *
+from multi_swe_bench.harness.repos.typescript.stakwork.sphinx_tribes_frontend import *
