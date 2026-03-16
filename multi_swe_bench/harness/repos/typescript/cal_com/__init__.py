f83c5e70d59b56c96e7d8e28b7d6c22ad4a8b944
KTanmay1 <tanmaytushar21@gmail.com>
2026-03-16 01:45:28 +0000

    feat(typescript): add calcom/cal.com repo config (2 eras) - cal_com_20489_to_9422: vitest era (PRs 9422-20489), node:20, yarn berry (corepack), vitest+turbo - cal_com_8506_to_7166: jest era (PRs 7166-8506), node:18, yarn classic, jest - Unique image tags: base-vitest, base-jest - Replaces old cal_com/ directory and old calcom era configs

--- multi_swe_bench/harness/repos/typescript/cal_com/__init__.py
+++
@@ -1,6 +0,0 @@
-import os
-from multi_swe_bench.harness.instance import Instance
-from .base import CalcomInstance
-
-# Dynamic registration of calcom/cal.com instances
-Instance.register("calcom", "cal.com")(CalcomInstance)
