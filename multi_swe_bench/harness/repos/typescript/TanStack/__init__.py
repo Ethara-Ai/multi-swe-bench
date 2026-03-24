f38f7557f54ceb6fb1e39c390413d5a15f2ac5e1
Abhishekethar <abhishek.verma@ethara.ai>
2026-03-24 13:57:31 +0530

    update repo TanStack

--- multi_swe_bench/harness/repos/typescript/TanStack/__init__.py
+++ multi_swe_bench/harness/repos/typescript/TanStack/__init__.py
@@ -1,2 +1,4 @@
 from multi_swe_bench.harness.repos.typescript.TanStack.router_2679_to_2543 import *
 from multi_swe_bench.harness.repos.typescript.TanStack.router_2543_to_1490 import *
+from multi_swe_bench.harness.repos.typescript.TanStack.table_5071_to_4061 import *
+from multi_swe_bench.harness.repos.typescript.TanStack.table_5989_to_5276 import *
