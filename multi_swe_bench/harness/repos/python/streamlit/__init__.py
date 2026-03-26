c478ce4352b167679b36c8869f23389008942790
Abhijeet <abhijeet.chaurasiya@ethara.ai>
2026-03-26 07:49:41 +0000

    Modified and added few configs in streamlit registry

--- multi_swe_bench/harness/repos/python/streamlit/__init__.py
+++ multi_swe_bench/harness/repos/python/streamlit/__init__.py
@@ -14,3 +14,7 @@ try:
     from multi_swe_bench.harness.repos.python.streamlit.streamlit_8064_to_7470 import *
 except (ImportError, ModuleNotFoundError):
     pass
+from multi_swe_bench.harness.repos.python.streamlit.streamlit_10110_to_7544 import *
+from multi_swe_bench.harness.repos.python.streamlit.streamlit_13510_to_11911 import *
+from multi_swe_bench.harness.repos.python.streamlit.streamlit_12767_to_12767 import *
+from multi_swe_bench.harness.repos.python.streamlit.streamlit_14289_to_13833 import *
