4d4825c8ce71ed1fe1419f972e867a2ff415ff59
kshitijbhardwaj-kb-ethara <kshitij.bhardwaj@ethara.ai>
2026-03-27 13:08:11 +0000

    Add a registry sharkdp_hyperfine

--- multi_swe_bench/harness/repos/rust/sharkdp/__init__.py
+++ multi_swe_bench/harness/repos/rust/sharkdp/__init__.py
@@ -1,5 +1,6 @@
 from multi_swe_bench.harness.repos.rust.sharkdp.bat import *
 from multi_swe_bench.harness.repos.rust.sharkdp.fd import *
+from multi_swe_bench.harness.repos.rust.sharkdp.hyperfine import *
 from multi_swe_bench.harness.repos.rust.sharkdp.bat_3068_to_1971 import *
 from multi_swe_bench.harness.repos.rust.sharkdp.fd_1540_to_233 import *
 from multi_swe_bench.harness.repos.rust.sharkdp.fd_233_to_58 import *
