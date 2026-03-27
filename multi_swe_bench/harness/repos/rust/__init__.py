8e98d3c4807fc7c14d0a134809583728322425a1
Shamy <shamy.ahmad@ethara.ai>
2026-03-27 12:12:55 +0000

    Added tauri-apps/tauri registry

--- multi_swe_bench/harness/repos/rust/__init__.py
+++ multi_swe_bench/harness/repos/rust/__init__.py
@@ -21,6 +21,7 @@ from multi_swe_bench.harness.repos.rust.serde_rs import *
 from multi_swe_bench.harness.repos.rust.sharkdp import *
 from multi_swe_bench.harness.repos.rust.starship import *
 from multi_swe_bench.harness.repos.rust.tailcallhq import *
+from multi_swe_bench.harness.repos.rust.tauri_apps import *
 from multi_swe_bench.harness.repos.rust.tokio_rs import *
 from multi_swe_bench.harness.repos.rust.unicode_org import *
 from multi_swe_bench.harness.repos.rust.actix import *
---
+++ multi_swe_bench/harness/repos/rust/tauri_apps/__init__.py
@@ -0,0 +1,16 @@
+try:
+    from multi_swe_bench.harness.repos.rust.tauri_apps.tauri_258_to_167 import *
+except (ImportError, ModuleNotFoundError):
+    pass
+try:
+    from multi_swe_bench.harness.repos.rust.tauri_apps.tauri_13454_to_3934 import *
+except (ImportError, ModuleNotFoundError):
+    pass
+try:
+    from multi_swe_bench.harness.repos.rust.tauri_apps.tauri_11329_to_11191 import *
+except (ImportError, ModuleNotFoundError):
+    pass
+try:
+    from multi_swe_bench.harness.repos.rust.tauri_apps.tauri_15030_to_14662 import *
+except (ImportError, ModuleNotFoundError):
+    pass
