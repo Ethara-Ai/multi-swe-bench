0a5c7989aee81a3cf1927038c421d85fd56a15a5
sagaragrahari-cell <sagar.agrahari@ethara.ai>
2026-03-30 08:14:05 +0000

    added registry for microsoft_pyright

--- multi_swe_bench/harness/repos/typescript/microsoft/__init__.py
+++ multi_swe_bench/harness/repos/typescript/microsoft/__init__.py
@@ -1,2 +1,5 @@
 from multi_swe_bench.harness.repos.typescript.microsoft.vscode import *
 from multi_swe_bench.harness.repos.typescript.microsoft.TypeScript import *
+from multi_swe_bench.harness.repos.typescript.microsoft.pyright_5584_to_2913 import *
+from multi_swe_bench.harness.repos.typescript.microsoft.pyright_7138_to_5627 import *
+from multi_swe_bench.harness.repos.typescript.microsoft.pyright_10984_to_7298 import *
