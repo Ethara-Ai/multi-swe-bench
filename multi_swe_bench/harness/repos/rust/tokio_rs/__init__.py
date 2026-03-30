17052bea3f6af42f4be3c4e33a06c939511736d1
Rajni Kant Singh <rajni.kant@ethara.ai>
2026-03-30 10:27:39 +0000

    Added registry for tokio_rs

--- multi_swe_bench/harness/repos/rust/tokio_rs/__init__.py
+++ multi_swe_bench/harness/repos/rust/tokio_rs/__init__.py
@@ -4,3 +4,4 @@ from multi_swe_bench.harness.repos.rust.tokio_rs.tokio import *
 from multi_swe_bench.harness.repos.rust.tokio_rs.tracing import *
 from multi_swe_bench.harness.repos.rust.tokio_rs.tokio_6229_to_4434 import *
 from multi_swe_bench.harness.repos.rust.tokio_rs.tracing_2442_to_853 import *
+from multi_swe_bench.harness.repos.rust.tokio_rs.prost import *
