bc3491d592b4ec4c76212d9ddd78fbcfe692c726
abhishekshawethara <abhishek.shaw@ethara.ai>
2026-03-30 12:26:20 +0000

    added pmndrs config files

--- multi_swe_bench/harness/repos/typescript/pmndrs/__init__.py
+++ multi_swe_bench/harness/repos/typescript/pmndrs/__init__.py
@@ -1,3 +1,6 @@
 from multi_swe_bench.harness.repos.typescript.pmndrs.react_spring_375_to_114 import *
 from multi_swe_bench.harness.repos.typescript.pmndrs.react_spring_2040_to_464 import *
 from multi_swe_bench.harness.repos.typescript.pmndrs.react_spring_2368_to_2091 import *
+from multi_swe_bench.harness.repos.typescript.pmndrs.jotai import *
+from multi_swe_bench.harness.repos.typescript.pmndrs.jotai_2492_to_1919 import *
+from multi_swe_bench.harness.repos.typescript.pmndrs.jotai_3264_to_2448 import *
