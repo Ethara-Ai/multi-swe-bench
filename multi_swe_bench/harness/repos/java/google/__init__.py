537ee2cb3f30e07cb07d8545e1d592b3eb7fda06
abhishekshawethara <abhishek.shaw@ethara.ai>
2026-04-07 12:44:16 +0000

    created google guice config files

--- multi_swe_bench/harness/repos/java/google/__init__.py
+++ multi_swe_bench/harness/repos/java/google/__init__.py
@@ -1,1 +1,3 @@
 from multi_swe_bench.harness.repos.java.google.gson import *
+from multi_swe_bench.harness.repos.java.google.guice_1086_to_1685 import *
+from multi_swe_bench.harness.repos.java.google.guice_1789_to_1906 import *
