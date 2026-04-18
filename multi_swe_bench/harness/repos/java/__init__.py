b7ad37e3901da02e860dcf61a72c22038daac9ab
Nitish tiwari <nitish.tiwari@ethara.ai>
2026-04-18 07:06:47 +0000

    Added: new registry - java/TheAlgorithms

--- multi_swe_bench/harness/repos/java/__init__.py
+++ multi_swe_bench/harness/repos/java/__init__.py
@@ -19,3 +19,4 @@ from multi_swe_bench.harness.repos.java.JodaOrg import *
 from multi_swe_bench.harness.repos.java.micronautprojects import *
 from multi_swe_bench.harness.repos.java.opentracing import *
 from multi_swe_bench.harness.repos.java.springprojects import *
+from multi_swe_bench.harness.repos.java.TheAlgorithms import *
