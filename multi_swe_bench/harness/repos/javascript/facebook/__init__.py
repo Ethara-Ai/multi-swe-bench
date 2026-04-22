50917c3a435d1180a46f00dd5f17a2d1fd6f20e1
harshakshsingh-arch <harshaksh.singh@ethara.ai>
2026-04-22 12:12:55 +0000

    Added New registry for Javascript/facebook

--- multi_swe_bench/harness/repos/javascript/facebook/__init__.py
+++ multi_swe_bench/harness/repos/javascript/facebook/__init__.py
@@ -1,1 +1,4 @@
 from multi_swe_bench.harness.repos.javascript.facebook.react import *
+from multi_swe_bench.harness.repos.javascript.facebook.create_react_app_0_to_3717 import *
+from multi_swe_bench.harness.repos.javascript.facebook.create_react_app_3718_to_13737 import *
+from multi_swe_bench.harness.repos.javascript.facebook.create_react_app_13738_to_99999 import *
