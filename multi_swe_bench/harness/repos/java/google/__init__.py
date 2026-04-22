edc672450747a16210ae3546cd295f1b08b655e6
harshakshsingh-arch <harshaksh.singh@ethara.ai>
2026-04-22 12:08:12 +0000

    Added new registry for Java/google/guava.py

--- multi_swe_bench/harness/repos/java/google/__init__.py
+++ multi_swe_bench/harness/repos/java/google/__init__.py
@@ -1,3 +1,4 @@
 from multi_swe_bench.harness.repos.java.google.gson import *
+from multi_swe_bench.harness.repos.java.google.guava import *
 from multi_swe_bench.harness.repos.java.google.guice_1086_to_1685 import *
 from multi_swe_bench.harness.repos.java.google.guice_1789_to_1906 import *
