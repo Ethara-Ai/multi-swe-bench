9f076a979b00c81989a751aa30688bddc89a23c3
minukumari01 <minu.kumari@ethara.ai>
2026-04-25 14:03:27 +0000

    created registry for rust_lang

--- multi_swe_bench/harness/repos/rust/rust_lang/__init__.py
+++ multi_swe_bench/harness/repos/rust/rust_lang/__init__.py
@@ -1,2 +1,7 @@
 from multi_swe_bench.harness.repos.rust.rust_lang.mdBook import *
 from multi_swe_bench.harness.repos.rust.rust_lang.rust import *
+from multi_swe_bench.harness.repos.rust.rust_lang.rust_analyzer import *
+from multi_swe_bench.harness.repos.rust.rust_lang.rust_analyzer_7055_to_4676 import *
+from multi_swe_bench.harness.repos.rust.rust_lang.rust_analyzer_7620_to_7062 import *
+from multi_swe_bench.harness.repos.rust.rust_lang.rust_analyzer_15857_to_9417 import *
+from multi_swe_bench.harness.repos.rust.rust_lang.rust_analyzer_16066_to_15887 import *
