de764e215c04aac131d0e4d6fb7a206f1dd5b1f0
minukumari01 <minu.kumari@ethara.ai>
2026-04-25 12:52:22 +0000

    created registry for golang

---
+++ multi_swe_bench/harness/repos/golang/go_task/__init__.py
@@ -0,0 +1,6 @@
+from multi_swe_bench.harness.repos.golang.go_task.task_premod import *
+from multi_swe_bench.harness.repos.golang.go_task.task_go1_13 import *
+from multi_swe_bench.harness.repos.golang.go_task.task_go1_16 import *
+from multi_swe_bench.harness.repos.golang.go_task.task_go1_18 import *
+from multi_swe_bench.harness.repos.golang.go_task.task_go1_20 import *
+from multi_swe_bench.harness.repos.golang.go_task.task_go1_22 import *
