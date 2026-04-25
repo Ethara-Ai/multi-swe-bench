29ea59f12c6cde5d7433fdd07871c8e62c7d7805
ankitakumari52-pixel <ankita.kumari52@ethara.ai>
2026-04-25 11:44:24 +0000

    Created registry for Z3Prover

---
+++ multi_swe_bench/harness/repos/cpp/Z3Prover/__init__.py
@@ -0,0 +1,1 @@
+from multi_swe_bench.harness.repos.cpp.Z3Prover.z3 import *
--- multi_swe_bench/harness/repos/cpp/__init__.py
+++ multi_swe_bench/harness/repos/cpp/__init__.py
@@ -22,8 +22,9 @@ from multi_swe_bench.harness.repos.cpp.microsoft import *
 from multi_swe_bench.harness.repos.cpp.ericniebler import *
 from multi_swe_bench.harness.repos.cpp.uNetworking import *
 from multi_swe_bench.harness.repos.cpp.doctest import *
-# from multi_swe_bench.harness.repos.cpp.danmar import *
 from multi_swe_bench.harness.repos.cpp.MariaDB import *
 from multi_swe_bench.harness.repos.cpp.Neargye import *
 from multi_swe_bench.harness.repos.cpp.polybar import *
 from multi_swe_bench.harness.repos.cpp.lballabio import *
+# from multi_swe_bench.harness.repos.cpp.danmar import *  # directory missing
+from multi_swe_bench.harness.repos.cpp.Z3Prover import *
