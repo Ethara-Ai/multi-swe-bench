031d21b27f5e931a8f294d084bd88cc3d97bf936
Abhirajgovind <abhiraj.govind@ethara.ai>
2026-04-26 10:55:24 +0000

    created the registry for serverless

--- multi_swe_bench/harness/repos/javascript/serverless/__init__.py
+++ multi_swe_bench/harness/repos/javascript/serverless/__init__.py
@@ -1,1 +1,2 @@
-from multi_swe_bench.harness.repos.javascript.serverless.serverless import *
+from multi_swe_bench.harness.repos.javascript.serverless.serverless_13185_to_3037 import *
+from multi_swe_bench.harness.repos.javascript.serverless.serverless_13476_to_13194 import *
