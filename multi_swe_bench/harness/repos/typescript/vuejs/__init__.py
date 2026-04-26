65af49a8a1e2b4eeae3f4485436fa122a1a88bf6
Abhirajgovind <abhiraj.govind@ethara.ai>
2026-04-26 10:53:16 +0000

    uploaded the vuejs init file

--- multi_swe_bench/harness/repos/typescript/vuejs/__init__.py
+++ multi_swe_bench/harness/repos/typescript/vuejs/__init__.py
@@ -1,3 +1,5 @@
 from multi_swe_bench.harness.repos.typescript.vuejs.core import *
+from multi_swe_bench.harness.repos.typescript.vuejs.core_pnpm_jest import *
+from multi_swe_bench.harness.repos.typescript.vuejs.core_yarn_jest import *
 from multi_swe_bench.harness.repos.typescript.vuejs.eslint_plugin_vue import *
 from multi_swe_bench.harness.repos.typescript.vuejs.vue import *
