1697cc8c812201768a71375986739402c6827b3b
samyakjain399 <samyak.jain@ethara.ai>
2026-04-27 10:16:55 +0000

    Resolved merged conflicts of rust init file

--- multi_swe_bench/harness/repos/rust/__init__.py
+++ multi_swe_bench/harness/repos/rust/__init__.py
@@ -54,3 +54,4 @@ from multi_swe_bench.harness.repos.rust.async_graphql import *
 from multi_swe_bench.harness.repos.rust.dtolnay import *
 from multi_swe_bench.harness.repos.rust.tower_rs import *
 from multi_swe_bench.harness.repos.rust.iced_rs import *
+from multi_swe_bench.harness.repos.rust.rustdesk import *
