900b1962bd7adf7a19cf7c72fc282d83b36e9605
Abhay Gandotra <abhay.gandotra.int12@ethara.ai>
2026-04-27 05:52:49 +0000

    created registry for atuinsh

---
+++ multi_swe_bench/harness/repos/rust/atuinsh/__init__.py
@@ -0,0 +1,4 @@
+from multi_swe_bench.harness.repos.rust.atuinsh.atuin import *
+from multi_swe_bench.harness.repos.rust.atuinsh.atuin_1425_to_646 import *
+from multi_swe_bench.harness.repos.rust.atuinsh.atuin_2067_to_2067 import *
+from multi_swe_bench.harness.repos.rust.atuinsh.atuin_28_to_28 import *
