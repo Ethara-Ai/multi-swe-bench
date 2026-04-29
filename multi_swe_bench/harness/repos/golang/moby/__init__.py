e5c205a981dee80241b9fad0d53cfef724d06d76
srashtijadon <srashti.jadon2308@ethara.ai>
2026-04-29 14:26:16 +0000

    added registry for genqlient , buildkit , agentscope

--- multi_swe_bench/harness/repos/golang/moby/__init__.py
+++ multi_swe_bench/harness/repos/golang/moby/__init__.py
@@ -1,1 +1,2 @@
 from multi_swe_bench.harness.repos.golang.moby.moby import *
+from multi_swe_bench.harness.repos.golang.moby.buildkit import *
--- multi_swe_bench/harness/repos/java/__init__.py
+++ multi_swe_bench/harness/repos/java/__init__.py
@@ -30,3 +30,4 @@ from multi_swe_bench.harness.repos.java.openequella import *
 from multi_swe_bench.harness.repos.java.TeamNewPipe.NewPipe import *
 from multi_swe_bench.harness.repos.java.TeamNewPipe.NewPipe_5927_to_5927 import *
 from multi_swe_bench.harness.repos.java.termux import *
+from multi_swe_bench.harness.repos.java.agentscope_ai import *
