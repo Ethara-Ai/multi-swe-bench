bfe4d7682a78bf78b116a3df53598a4555a645d6
deekshasinghint4-cpu <deeksha.singh.int4@ethara.ai>
2026-04-29 19:18:28 +0530

    added repo query_string

--- multi_swe_bench/harness/repos/javascript/sindresorhus/__init__.py
+++ multi_swe_bench/harness/repos/javascript/sindresorhus/__init__.py
@@ -2,3 +2,4 @@ from multi_swe_bench.harness.repos.javascript.sindresorhus.eslint_plugin_unicorn
 from multi_swe_bench.harness.repos.javascript.sindresorhus.linkify_urls import *
 from multi_swe_bench.harness.repos.javascript.sindresorhus.globby import *
 from multi_swe_bench.harness.repos.javascript.sindresorhus.meow import *
+from multi_swe_bench.harness.repos.javascript.sindresorhus.query_string import *
