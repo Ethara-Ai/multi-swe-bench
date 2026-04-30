7d488ac627aa4f35629c6865d1afadef859bb10e
farkanshgupta <farkansh.gupta22@ethara.ai>
2026-04-30 06:39:16 +0000

    Added RaspberryPiFoundation, eslint_plugin_jsx_a11y, sindresorhuselement_ready, sindresorhus file_type to codebase

--- multi_swe_bench/harness/repos/javascript/sindresorhus/__init__.py
+++ multi_swe_bench/harness/repos/javascript/sindresorhus/__init__.py
@@ -1,5 +1,7 @@
 from multi_swe_bench.harness.repos.javascript.sindresorhus.eslint_plugin_unicorn import *
+from multi_swe_bench.harness.repos.javascript.sindresorhus.file_type import *
 from multi_swe_bench.harness.repos.javascript.sindresorhus.linkify_urls import *
 from multi_swe_bench.harness.repos.javascript.sindresorhus.globby import *
 from multi_swe_bench.harness.repos.javascript.sindresorhus.meow import *
 from multi_swe_bench.harness.repos.javascript.sindresorhus.query_string import *
+from multi_swe_bench.harness.repos.javascript.sindresorhus.element_ready import *
