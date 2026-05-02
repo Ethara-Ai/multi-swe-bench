019e818608c43cbdf8d40cdd68e9f73eae187b9e
Abhirajgovind <abhiraj.govind@ethara.ai>
2026-05-02 05:46:46 +0000

    created registry for nuxt

--- multi_swe_bench/harness/repos/typescript/nuxt/__init__.py
+++ multi_swe_bench/harness/repos/typescript/nuxt/__init__.py
@@ -1,1 +1,3 @@
 from multi_swe_bench.harness.repos.typescript.nuxt.nuxt import *
+from multi_swe_bench.harness.repos.typescript.nuxt.nuxt_2736_to_147 import *
+from multi_swe_bench.harness.repos.typescript.nuxt.nuxt_10193_to_3683 import *
