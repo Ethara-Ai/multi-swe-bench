ef8fe36bd515d25ee93c244573f685ccfdefa166
Abhirajgovind <abhiraj.govind@ethara.ai>
2026-05-02 05:53:51 +0000

    created registry for n8n-io

---
+++ multi_swe_bench/harness/repos/typescript/n8n_io/__init__.py
@@ -0,0 +1,3 @@
+from multi_swe_bench.harness.repos.typescript.n8n_io.n8n_3756_to_4 import *
+from multi_swe_bench.harness.repos.typescript.n8n_io.n8n_4459_to_2739 import *
+from multi_swe_bench.harness.repos.typescript.n8n_io.n8n_5376_to_3835 import *
