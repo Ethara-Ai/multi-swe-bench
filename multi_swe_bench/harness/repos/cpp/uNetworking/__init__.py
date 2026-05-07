bd029a00b48a31128d278a7e0e094eca0d175cc8
abhijeet-ethara <abhijeet.chaurasiya@ethara.ai>
2026-05-07 19:52:01 +0000

    Fixed uNetworking imports file

--- multi_swe_bench/harness/repos/cpp/uNetworking/__init__.py
+++ multi_swe_bench/harness/repos/cpp/uNetworking/__init__.py
@@ -1,20 +1,5 @@
-try:
-    from multi_swe_bench.harness.repos.cpp.uNetworking.uWebSockets_503_to_402 import *
-except (ImportError, ModuleNotFoundError):
-    pass
-try:
-    from multi_swe_bench.harness.repos.cpp.uNetworking.uWebSockets_749_to_679 import *
-except (ImportError, ModuleNotFoundError):
-    pass
-try:
-    from multi_swe_bench.harness.repos.cpp.uNetworking.uWebSockets_924_to_879 import *
-except (ImportError, ModuleNotFoundError):
-    pass
-try:
-    from multi_swe_bench.harness.repos.cpp.uNetworking.uWebSockets_1383_to_1249 import *
-except (ImportError, ModuleNotFoundError):
-    pass
-try:
-    from multi_swe_bench.harness.repos.cpp.uNetworking.uWebSockets_1862_to_1712 import *
-except (ImportError, ModuleNotFoundError):
-    pass
+from multi_swe_bench.harness.repos.cpp.uNetworking.uWebSockets_503_to_402 import *
+from multi_swe_bench.harness.repos.cpp.uNetworking.uWebSockets_749_to_679 import *
+from multi_swe_bench.harness.repos.cpp.uNetworking.uWebSockets_924_to_879 import *
+from multi_swe_bench.harness.repos.cpp.uNetworking.uWebSockets_1383_to_1249 import *
+from multi_swe_bench.harness.repos.cpp.uNetworking.uWebSockets_1862_to_1712 import *
