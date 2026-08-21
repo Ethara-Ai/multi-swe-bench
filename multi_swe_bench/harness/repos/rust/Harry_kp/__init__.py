5d89d6a470592ff1c81e1e2b874abd3c0f380816
kshitijbhardwaj-kb-ethara <kshitij.bhardwaj@ethara.ai>
2026-08-21 18:44:51 +0530

    Added Registery

---
+++ multi_swe_bench/harness/repos/rust/Harry_kp/__init__.py
@@ -0,0 +1,1 @@
+from multi_swe_bench.harness.repos.rust.Harry_kp.vortix import *
--- multi_swe_bench/harness/repos/rust/__init__.py
+++ multi_swe_bench/harness/repos/rust/__init__.py
@@ -75,3 +75,4 @@ from multi_swe_bench.harness.repos.rust.apache import *
 
 import importlib as _importlib
 # _importlib.import_module("multi_swe_bench.harness.repos.rust.InterCooperative-Network")  # module not present
+from multi_swe_bench.harness.repos.rust.Harry_kp import *
