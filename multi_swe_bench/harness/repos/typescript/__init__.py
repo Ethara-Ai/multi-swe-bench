7f6c52aabcca3ddd460699d854c6e129590d51fe
anchalmittal-27 <anchal.mittal@ethara.ai>
2026-08-24 16:50:06 +0530

    Added aurelia repo config in typescript

--- multi_swe_bench/harness/repos/typescript/__init__.py
+++ multi_swe_bench/harness/repos/typescript/__init__.py
@@ -203,3 +203,4 @@ from multi_swe_bench.harness.repos.typescript.ForNeVeR import *
 from multi_swe_bench.harness.repos.typescript.zowe import *
 from multi_swe_bench.harness.repos.typescript.neos import *
 from multi_swe_bench.harness.repos.typescript.the_guild_org import *
+from multi_swe_bench.harness.repos.typescript.aurelia import *
