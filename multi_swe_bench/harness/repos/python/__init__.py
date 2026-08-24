f19266db31b1c79158e27fefbe1b295cd8e71acc
nirmalethara <nirmal.gandhwani@ethara.ai>
2026-08-24 18:09:11 +0530

    Updated Repo config file for Python-MozillaSecurity and Golang-bazelbuild

--- multi_swe_bench/harness/repos/python/__init__.py
+++ multi_swe_bench/harness/repos/python/__init__.py
@@ -385,3 +385,4 @@ from multi_swe_bench.harness.repos.python.wpilibsuite import *
 from multi_swe_bench.harness.repos.python.treeandsea import *
 from multi_swe_bench.harness.repos.python.LukeCarrier import *
 from multi_swe_bench.harness.repos.python.lyz_code import *
+from multi_swe_bench.harness.repos.python.MozillaSecurity import *
