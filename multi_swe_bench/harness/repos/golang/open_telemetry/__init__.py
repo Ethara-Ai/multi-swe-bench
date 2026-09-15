2b66ae6520213fb4cf7575cca7c636664726bf94
Abhishek Kumar <abhishek.kumar1151@ethara.ai>
2026-09-15 19:13:57 +0530

    fix repo config

--- multi_swe_bench/harness/repos/golang/open_telemetry/__init__.py
+++ multi_swe_bench/harness/repos/golang/open_telemetry/__init__.py
@@ -1,7 +1,4 @@
 from multi_swe_bench.harness.repos.golang.open_telemetry.opentelemetry_collector import *
-<<<<<<< Updated upstream
 from multi_swe_bench.harness.repos.golang.open_telemetry.opentelemetry_go import *
-=======
 from multi_swe_bench.harness.repos.golang.open_telemetry.open_telemetry import *
 from multi_swe_bench.harness.repos.golang.open_telemetry.opentelemetry_go_contrib_621 import *
->>>>>>> Stashed changes
