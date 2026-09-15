f7da4b30b43980d14661e32184de93d3d1d170ca
Abhishek Kumar <abhishek.kumar1151@ethara.ai>
2026-09-15 12:24:47 +0530

    fix repo config

--- multi_swe_bench/harness/repos/python/__init__.py
+++ multi_swe_bench/harness/repos/python/__init__.py
@@ -431,7 +431,6 @@ from multi_swe_bench.harness.repos.python.pwndbg import *
 
 from multi_swe_bench.harness.repos.python.CycloneDX import *
 
-<<<<<<< Updated upstream
 from multi_swe_bench.harness.repos.python.Spherre_Labs import *
 from multi_swe_bench.harness.repos.python.Tuxemon import *
 from multi_swe_bench.harness.repos.python.SolFoundry import *
@@ -439,6 +438,4 @@ from multi_swe_bench.harness.repos.python.ansible_collections import *
 
 from multi_swe_bench.harness.repos.python.stumpy_dev import *
 
-=======
 from multi_swe_bench.harness.repos.python.jannikmi import *
->>>>>>> Stashed changes
