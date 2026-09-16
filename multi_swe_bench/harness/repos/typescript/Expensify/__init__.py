2021dc677a741c6cbf0fafc177ff16569b53c4dc
aayushdixit-ethara <aayush.dixit@ethara.ai>
2026-09-16 14:55:07 +0530

    Added Registry for Expensify in Typescript

--- multi_swe_bench/harness/repos/typescript/Expensify/__init__.py
+++ multi_swe_bench/harness/repos/typescript/Expensify/__init__.py
@@ -2,3 +2,4 @@ from multi_swe_bench.harness.repos.typescript.Expensify.App import *
 from multi_swe_bench.harness.repos.typescript.Expensify.App_76743_to_72955 import *
 from multi_swe_bench.harness.repos.typescript.Expensify.App_78894_to_76836 import *
 from multi_swe_bench.harness.repos.typescript.Expensify.App_79530_to_78986 import *
+from multi_swe_bench.harness.repos.typescript.Expensify.App_83813_to_83517 import *
